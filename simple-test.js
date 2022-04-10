5563e44d443a7dd4094b0940c746d7008130212c
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-04-10 12:15:49 +0800

    docs: ✏️ update readme

--- simple-test.js
+++ simple-test.js
@@ -10,19 +10,18 @@ const m = new FileMap()
 m.addFile('dist/examples/index.js', index)
 m.addFile('dist/examples/result.js', result)
 
-let diagnostic = Diagnostic.warning()
-
+let diagnostic = Diagnostic.error()
 diagnostic.withMessage('Something wrong')
 diagnostic.withLabels([
   primaryDiagnosticLabel(m.getFileId('dist/examples/result.js'), {
     start: positionToOffset(result, 1, 6),
     end: positionToOffset(result, 1, 7),
-    message: "Variable a can't be redefined",
+    message: "Variable `a` can't be redefined",
   }),
   secondaryDiagnosticLabel(m.getFileId('dist/examples/result.js'), {
     start: positionToOffset(result, 0, 4),
     end: positionToOffset(result, 0, 5),
-    message: 'Variable a first defined here',
+    message: 'Variable `a` first defined here',
   }),
 ])
 
